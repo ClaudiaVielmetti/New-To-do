155a3c648ffedfe84993a474ff2bbad71b211457
ClaudiaV27 <claudiavielmetti38@gmail.com>
2022-10-17 15:37:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ form.addEventListener('submit' , function (event) {
     localStorage.setItem('todos' , JSON.stringify(todos));
 
 });
+
+//Save todos
+function saveTodo() {
+    const todoValue = todoInput.ariaValue;
+
+    //check if value is empty
+    const isEmpty = todoValue === '';
+
+    //check for duplicate entries
+    const isDuplicate = todos.some((todo) => todo.value.toUpperCase() === todoValue.toUpperCase());
+
+    if (isEmpty) {
+        showNotification("Todo input is empty");
+    
+    } else if (isDuplicate) {
+        showNotification("Task already exists!");
+    } else {
+        if (EditTodoId >=0) {
+            todos = todos.map((todo, index) => ({
+                ...todo,
+                value: index === EditTodoId ? todoValue : todo.value,
+            }));
+            
+            EditTodoId = -1;
+        } else {
+          todos.push({
+            value: todoValue,
+            checked: false,
+            color: '#' + Math.floor(Math.random() * 16777215).toString(16),
+          }); 
+        
+        }
+
+        todoInput.value = '';
+    }
+}

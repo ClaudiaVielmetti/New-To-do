17da9651e577a915435aa4e1a774e6a3da96cb3f
ClaudiaV27 <claudiavielmetti38@gmail.com>
2022-10-24 16:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ function saveTodo() {
   }
 }
 
+
 $(document).ready(function(){
   var date_input=$('input[name="date"]'); //our date input has the name "date"
   var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
